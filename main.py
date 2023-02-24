66409d3ae0512965e111b2897c78433b1070fee4
wouterton <126216067+wouterton@users.noreply.github.com>
2023-02-24 03:03:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os
 
 ip = input('IP: ')
-port = input('PORT: ')
-time = input('TIME:')
+
 os.system('clear')
 
-os.system(f'perl main.pl {ip} {port} 66500 {time}')
+os.system(f'perl main.pl {ip}')
 
 
